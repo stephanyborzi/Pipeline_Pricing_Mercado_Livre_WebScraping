47284c922e8065da3fa93ede234e87020c181598
unknown <borzistephany18@outlook.com>
2024-12-27 17:18:35 -0300

    Todos os requisitos de negócio já foram extraídos

--- coleta/coleta/spiders/mercadolivre.py
+++ coleta/coleta/spiders/mercadolivre.py
@@ -15,7 +15,15 @@ class MercadolivreSpider(scrapy.Spider):
     def parse(self, response):
         products = response.css('div.poly-card__content')
         for product in products:
+            old_price_parts = product.css('s.andes-money-amount--previous span::text').getall()
+            old_price = ''.join(old_price_parts).replace('R$', '').strip()
+
             yield {
-                'brand': product.css('span.poly-component__brand::text').get()
+                'brand': product.css('span.poly-component__brand::text').get(),
+                'name': product.css('h2.poly-box.poly-component__title a::text').get(),
+                'old_price': old_price,
+                'new_price': ''.join(product.css('span.andes-money-amount--cents-superscript span::text').getall()).replace('R$', '').strip(),
+                'review_amount': product.css('span.poly-reviews__rating::text').get(),
+                'review_rating_number': product.css('span.poly-reviews__total::text').re_first(r'\d+')
             }
 
