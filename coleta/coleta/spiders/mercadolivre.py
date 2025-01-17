70a99d523de70543c705590e738af26088f07a8d
unknown <borzistephany18@outlook.com>
2025-01-17 19:22:12 -0300

    Ajustando extração

--- coleta/coleta/spiders/mercadolivre.py
+++ coleta/coleta/spiders/mercadolivre.py
@@ -4,35 +4,19 @@ class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
     allowed_domains = ["mercadolivre.com.br"]
     start_urls = ["https://lista.mercadolivre.com.br/tenis-corrida-masculino"]
-    page_count = 1
-    max_page = 10
-
-    def start_requests(self):
-        headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) Gecko/20100101 Firefox/91.0'
-        }
-        for url in self.start_urls:
-            yield scrapy.Request(url, headers=headers)
-
+  
     def parse(self, response):
         products = response.css('div.poly-card__content')
         for product in products:
-            old_price_parts = product.css('s.andes-money-amount--previous span::text').getall()
-            old_price = ''.join(old_price_parts).replace('R$', '').strip()
-
+            prices = product.css('span.andes-money-amount__fraction::text').getall()
+            cents = product.css('span.andes-money-amount__cents::text').getall()
             yield {
                 'brand': product.css('span.poly-component__brand::text').get(),
-                'name': product.css('h2.poly-box.poly-component__title a::text').get(),
-                'old_price': old_price,
-                'new_price': ''.join(product.css('span.andes-money-amount--cents-superscript span::text').getall()).replace('R$', '').strip(),
+                'name': product.css('h2.poly-component__title-wrapper a::text').get(),
+                'old_price_reais': prices[0] if len(prices) > 0 else None,
+                'old_price_centavos': cents[0] if len(cents) > 0 else None,
+                'new_price_reais': prices[1] if len(prices) > 1 else None,
+                'new_price_centavos': cents[1] if len(cents) > 1 else None,
                 'review_amount': product.css('span.poly-reviews__rating::text').get(),
-                'review_rating_number': product.css('span.poly-reviews__total::text').re_first(r'\d+')
+                'review_rating_number': product.css('span.poly-reviews__total::text').get()
             }
-        
-        if self.page_count < self.max_page:
-            next_page = response.css('li.andes-pagination__button--next a::attr(href)').get()
-            if next_page:
-                self.page_count += 1
-                yield scrapy.Request(next_page, callback=self.parse)
-
-
