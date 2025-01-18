06620a2183a4e72757fded9a93d3abd71f3e9777
unknown <borzistephany18@outlook.com>
2025-01-17 22:42:16 -0300

    Atualiza o título do dashboard

--- src/dashboard/dashboard.py
+++ src/dashboard/dashboard.py
@@ -12,14 +12,13 @@ def testar_conexao():
         st.error(f"Erro ao conectar ao banco de dados: {e}")
         return False
 
-# Testar a conexão com o banco de dados
 if testar_conexao():
     st.success('Conexão com o banco de dados estabelecida com sucesso!')
     conn = sqlite3.connect('src/data/quotes.db')
     df = pd.read_sql_query('SELECT * FROM mercadolivre_items', conn)
     conn.close()
 
-    st.title('Pesquisa de Mercado - Tênis Esportivo Mercado Livre')
+    st.title('Oi Pesquisa de Mercado - Tênis Esportivo Mercado Livre')
     st.subheader('KPIs principais do sistema')
     
     col1, col2, col3 = st.columns(3)
@@ -30,14 +29,13 @@ if testar_conexao():
     avarage_new_price = df['preco_novo'].mean()
     col3.metric(label="Preço Médio Novo (R$)", value=f'{avarage_new_price:.2f}')
 
-    # Marcas mais encontradas até a 10ª página
+    
     st.subheader('Marcas mais encontradas até a 10ª página')
     col1, col2 = st.columns(2)
     top_10_pages_brands = df['marca'].value_counts().sort_values(ascending=False)
     col1.write(top_10_pages_brands)
     col2.bar_chart(top_10_pages_brands)
     
-    # Preço médio por marca
     st.subheader('Preço Médio por Marca')
     col1, col2 = st.columns(2)
     df_rating_reviews = df[df['preco_novo'] > 0]
@@ -45,7 +43,7 @@ if testar_conexao():
     col1.write(avarage_price_by_brand)
     col2.bar_chart(avarage_price_by_brand)
     
-    # Satisfação por marca
+
     st.subheader('Satisfação por marca')
     col1, col2 = st.columns(2)
     df_reviews = df[df['quantidade_reviews'] > 0]
